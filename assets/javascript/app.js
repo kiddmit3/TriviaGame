4ad2bc25e7d4c2d35674275da8bd9634b698c1e7
David Lac <kiddmit3@gmail.com>
2017-07-27 20:03:51 -0700

    final

---
+++ assets/javascript/app.js
@@ -0,0 +1,205 @@
+$(document).ready(function() {
+	var index = 0;
+	var countdownTimer = {
+		time : 30,
+		reset: function() {
+			this.time = 30;
+			$('.timer').html('<h3>' + this.time + ' seconds remaining</h3>');
+		},
+		start: function() {
+			counter = setInterval(countdownTimer.count, 1000);	
+		},
+		stop: function() {
+			clearInterval(counter);
+		},
+		count: function() {
+				countdownTimer.time--;
+				console.log(countdownTimer.time);
+//				$('.timer').html(countdownTimer.time);
+			if (countdownTimer.time >= 0) {
+				$('.timer').html('<h3>' + countdownTimer.time + ' seconds remaining</h3>');
+			}
+			else {
+				index++;
+				answerWrong();
+				countdownTimer.reset();
+				if (index < questionArray.length) {
+					loadQuestion(index);
+				} else {
+					$(".answerchoice").hide();
+					showScore();
+				}
+			}
+		}
+	};
+
+var correct = 0;
+var wrong = 0;
+var audio = new Audio("assets/schwifty.mp3");
+
+var q1 = {
+	question : 'Who was assulted by King Jellybean?',
+	possibleAnswers : ['A. Morty',
+				 'B. Summer',
+				 'C. Rick',
+				 'D. Jerry'],
+	flags : [true, false, false, false],
+	answer : 'A. Morty'
+};
+
+var q2 = {
+	question: 'What was Rick arrested for at the end of Season 2?',
+	possibleAnswers: ['A. genocide',
+				 'B. grand theft',
+				 'C. treason',
+				 'D. everything'],
+	flags : [false, false, false, true],
+	answer : 'B. Michael Crawford'
+};
+
+var q3 = {
+	question : 'Who shot Mr. Poopy Butthole?',
+	possibleAnswers : ['A. Rick',
+				 'B. Morty',
+				 'C. Beth',
+				 'D. A parasite'],
+	flags : [false, false, true, false],
+	answer : 'B. Hooter'
+};
+
+var q4 = {
+	question : 'What alias does Rick use in "Get Schwifty?',
+	possibleAnswers : ['A. Schezwan Sammy',
+				 'B. Schwiftinator',
+				 'C. Mr. Bulldops',
+				 'D. Dr. Mic Drop'],
+	flags : [false, false, true, false],
+	answer : 'A. Sinclair ZX80'
+};
+
+var q5 = {
+	question : 'If you think my top is cute, you cannot _____?',
+	possibleAnswers : ['A. shoot',
+				 'B. execute',
+				 'C. insitute',
+				 'D. dispute'],
+	flags : [false, true, false, false],
+	answer : 'B. Morocco'
+};
+
+var questionArray = [q1, q2, q3, q4, q5];
+
+function loadQuestion(questionSelection) {
+	console.log(questionSelection);
+	countdownTimer.reset();
+  $(".question").html("<h3>" + questionArray[questionSelection].question + "</h3>");
+  $("#buttonA").text(questionArray[questionSelection].possibleAnswers[0]).show();
+  $("#buttonB").text(questionArray[questionSelection].possibleAnswers[1]).show();
+  $("#buttonC").text(questionArray[questionSelection].possibleAnswers[2]).show();
+  $("#buttonD").text(questionArray[questionSelection].possibleAnswers[3]).show();
+//  getAnswer();  
+//  nextQuestion(index);
+}
+
+//function nextQuestion() {
+//	index = index++;
+//	console.log(index);
+//}
+
+function setup() {
+	index = 0;
+	$('.question').append('<button id="startButton">Start</button>');
+	$('#startButton').on('click', function() {
+		$(this).hide();
+		countdownTimer.start();
+	 	loadQuestion(index);
+	});
+}		
+
+function getAnswer() {
+
+//  nextQuestion();
+	$('.answerchoice').on('click', function() {
+	  console.log('alert', index);
+		index++;
+		console.log('click', index);
+		$(".question").text('');
+		$("#buttonA").text('');
+		$("#buttonB").text('');
+		$("#buttonC").text('');
+		$("#buttonD").text('');
+		loadQuestion();
+	})
+}
+
+function answerCorrect() {
+	correct++;
+	alert("Correct!");
+	console.log("correct");
+}
+
+function answerWrong() {
+	wrong++;
+	alert("Incorrect!");
+	console.log("wrong");
+}
+
+function showScore() {
+	$('.question').empty();
+	$('.question').append("<h2><p>" + correct + " correct</p></h2>");
+	$('.question').append("<h2><p>" + wrong + " incorrect</p></h2>");
+	countdownTimer.stop();
+	$('.timer').empty();
+
+}
+
+audio.play();
+setup();
+$('.answerchoice').on('click', function() {
+ console.log($(this));
+ if(this.id == 'buttonA') {
+ 	var answerChosen = 'A';
+ } else if(this.id == 'buttonB') {
+ 	answerChosen = 'B';
+ } else if (this.id == 'buttonC') {
+ 	answerChosen = 'C';
+ } else if (this.id == 'buttonD') {
+ 	answerChosen = 'D';
+ } 
+ if ((answerChosen == 'A') && (questionArray[index].flags[0] == true)) {
+ 	answerCorrect();
+ } else if (answerChosen == 'A') {
+ 	answerWrong();
+ }
+ if ((answerChosen == 'B') && (questionArray[index].flags[1] == true)) {
+ 	answerCorrect();
+ } else if (answerChosen == 'B') {
+ 	answerWrong();
+ }
+if ((answerChosen == 'C') && (questionArray[index].flags[2] == true)) {
+ 	answerCorrect();
+ } else if (answerChosen == 'C') {
+ 	answerWrong();
+ }
+if ((answerChosen == 'D') && (questionArray[index].flags[3] == true)) {
+ 	answerCorrect();
+ } else if (answerChosen == 'D') {
+ 	answerWrong();
+ }
+
+ $(".question").text('');
+ $("#buttonA").text('');
+ $("#buttonB").text('');
+ $("#buttonC").text('');
+ $("#buttonD").text('');
+ index++;
+ if (index < questionArray.length) {
+ 	loadQuestion(index);
+ } else {
+ 	$(".answerchoice").hide();
+ 	showScore();
+ }
+});
+
+
+});
